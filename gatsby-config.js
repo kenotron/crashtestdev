22007b8d29787c386ff2649002dce8d625a6aa3f
Ken <kchau@microsoft.com>
2019-08-16 07:54:34 -0700

    netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,30 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        /**
+         * One convention is to place your Netlify CMS customization code in a
+         * `src/cms` directory.
+         */
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Crash Test Dev`,
+    author: `Ken Chau`,
+    description: `Why do I need a description. For a site. The site IS a description.`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/kenneth_chau`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/kenotron`,
       },
     ],
   },
--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -7,10 +7,6 @@ import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    This is where <Styled.a href="http://example.com/">your name</Styled.a>
-    {` `}
-    goes.
-    <br />
-    Or whatever, you make the rules.
+    <Styled.a href="http://example.com/">Ken Chau</Styled.a>
   </Fragment>
 )
