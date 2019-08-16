b56acdb9ab170997562528ccfaf0d7a2b913ab7f
Ken <kchau@microsoft.com>
2019-08-16 15:55:42 -0700

    back to github source we go!

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,6 @@
 import CMS from "netlify-cms-app"
+import FileSystemBackendClass from "netlify-cms-backend-fs"
 import { MdxControl, MdxPreview } from "netlify-cms-widget-mdx"
 
+CMS.registerBackend("file-system", FileSystemBackendClass)
 CMS.registerWidget("mdx", MdxControl, MdxPreview)
