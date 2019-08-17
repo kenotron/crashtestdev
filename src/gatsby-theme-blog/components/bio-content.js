91d67d21f36afa65ccb51e92978f42df504efee2
Ken <kchau@microsoft.com>
2019-08-16 20:40:33 -0700

    no link

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,12 +1,7 @@
 import React, { Fragment } from "react"
-import { Styled } from "theme-ui"
 
 /**
  * Change the content to add your own bio
  */
 
-export default () => (
-  <Fragment>
-    <Styled.a href="http://example.com/">Ken Chau</Styled.a>
-  </Fragment>
-)
+export default () => <Fragment>Ken Chau</Fragment>
